4d456dcc0163cf655ed5b8d31c77e8399aebb64f
BuildTools <unconfigured@null.spigotmc.org>
2021-01-06 11:52:51 +0100

    final testing

--- src/main/java/com/example/demo/repository/ImageRepository.java
+++ src/main/java/com/example/demo/repository/ImageRepository.java
@@ -11,5 +11,5 @@ public interface ImageRepository extends JpaRepository<Image, String> {
     @Query("select u from Image u WHERE u.key = :key")
     Image findImagesByKey(@Param("key") String key);
     @Query("select u from Image u WHERE u.userEmail = :email")
-    Image findImagesByEmail(@Param("key") String email);
+    Image findImagesByEmail(@Param("email") String email);
 }
